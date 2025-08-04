bbeb84deef1b90bd1486feeefc5fc460935c3dee
van-zxc <cxj2103354@outlook.com>
2025-08-04 10:50:59 +0800

    update quartz.congig.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -59,7 +59,6 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
@@ -67,6 +66,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
